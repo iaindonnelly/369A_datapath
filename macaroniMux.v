4f46665e0afec2a41d5e84cc50f5be18a4f812b8
iaindonnelly <iaindonnelly@email.arizona.edu>
2017-11-08 15:22:55 -0700

    updating files

--- macaroniMux.v
+++ macaroniMux.v
@@ -23,18 +23,18 @@
 module macaroniMux(out, inA, inB,inC, sel,INSstr);
 
     output reg [31:0] out;
-    input [31:0] INSstr;
+    input [3:0] INSstr;
     input [31:0] inC;
     input [31:0] inA;
     input [27:0] inB;
     input [1:0]  sel;
     
-    always@(sel, inA, inB) begin
+    always@(*) begin
         if(sel == 0) begin 
          out <= inA;
         end
         else if (sel == 1) begin // changed
-            out <= {INSstr[31:28],inB};
+            out <= {INSstr,inB};
         end
         else if(sel == 2) begin
             out <= inC;
